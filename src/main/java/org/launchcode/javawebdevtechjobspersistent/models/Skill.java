e7abddf12d92c8d8b0e8adbd28c40a33051aab19
Joshua Mantooth <jam2767@gmail.com>
2021-03-24 01:02:02 -0500

    Updated Employer and Skill classes

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @Size(max = 300)
+    private String Skill;
+
+    public Skill() {}
+
+    public String getSkill() {
+        return Skill;
+    }
+
+    public void setSkill(String skill) {
+        Skill = skill;
+    }
 }
